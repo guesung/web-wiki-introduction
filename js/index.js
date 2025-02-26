24650534519519014d345074bea27583c056a558
guesung <gueit214@naver.com>
2025-02-26 15:38:06 +0900

    feat: 댓글 기능

--- js/index.js
+++ js/index.js
@@ -1,19 +1,52 @@
 console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
+  '%c' +
+    ' __      __  ______   __  __   ______     ' +
+    '\n' +
+    '/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    ' +
+    '\n' +
     "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
+    '\n' +
+    ' \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    ' +
+    '\n' +
+    '  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ ' +
+    '\n' +
+    '   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ ' +
+    '\n' +
     "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
+  'color: #d81b60; font-size: 16px; font-weight: bold;',
 );
 
-console.log("알맞은 스크립트를 작성하세요");
+const submitButton = document.querySelector('.submit');
+const cancelButton = document.querySelector('.cancel');
+
+const resetInput = () => {
+  document.querySelector('.comment-input').value = '';
+};
+
+submitButton.addEventListener('click', () => {
+  const commentInput = document.querySelector('.comment-input');
+  if (!commentInput) return;
+
+  const comment = commentInput?.value;
+
+  const commentList = document.querySelector('.comment-list');
+  const commentItem = document.createElement('li');
+  commentItem.innerHTML = `
+              <div class="comment-item">
+                <div class="comment-author">
+                  <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" width="24px" />
+                  <span>방문자</span>
+                </div>
+                <div class="comment-content">
+                  ${comment}
+                </div>
+              </div>
+              `;
+
+  commentList.appendChild(commentItem);
+
+  window.alert('댓글이 등록되었습니다');
+  resetInput();
+});
+
+cancelButton.addEventListener('click', resetInput);
